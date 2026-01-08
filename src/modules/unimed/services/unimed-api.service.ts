e0be19ea1984bf2f3c0f944c619cff16e37c0d09
Joao Campos <joao.campos@viacometa.com.br>
2026-01-08 08:39:13 -0400

    FEAT: ajusta função getToken

--- src/modules/unimed/services/unimed-api.service.ts
+++ src/modules/unimed/services/unimed-api.service.ts
@@ -4,9 +4,9 @@ import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class UnimedApiService {
-  private readonly apiUrl: string | undefined;
-  private readonly apiUser: string | undefined;
-  private readonly apiPassword: string | undefined;
+  private readonly apiUrl: string;
+  private readonly apiUser: string;
+  private readonly apiPassword: string;
 
   private token: string;
 
@@ -14,30 +14,36 @@ export class UnimedApiService {
   private readonly logger = new Logger(UnimedApiService.name);
 
   constructor(private configService: ConfigService) {
-    this.apiUrl = this.configService.get<string>('UNIMED_API_URL');
-    this.apiUser = 'cometa';
-    this.apiPassword = 'C0m3t42019';
+    this.apiUrl = this.configService.get<string>('UNIMED_API_URL') || '';
+    this.apiUser = this.configService.get<string>('UNIMED_API_USER') || '';
+    this.apiPassword =
+      this.configService.get<string>('UNIMED_API_PASSWORD') || '';
 
     this.apiClient = axios.create({
       baseURL: this.apiUrl,
       timeout: 5000,
-      headers: {
-        'Content-Type': 'application/json',
-      },
     });
   }
 
   async getToken() {
     try {
-      const response = await this.apiClient.post<string>('/Token/geratoken', {
-        usuario: this.apiUser,
-        senha: this.apiPassword,
-      });
+      const response = await this.apiClient.post<string>(
+        '/Token/geratoken',
+        {},
+        {
+          headers: {
+            usuario: this.apiUser,
+            senha: this.apiPassword,
+          },
+        },
+      );
       this.logger.log('Token obtido com sucesso', response.data);
 
       this.token = response.data;
+      return this.token;
     } catch (e) {
-      console.error(e);
+      this.logger.error('Erro ao obter token:', e.response?.data || e.message);
+      throw e;
     }
   }
 }
