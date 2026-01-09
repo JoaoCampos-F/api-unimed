789ef7bc233c868d2c5c717c715b04dc3e56f589
Joao Campos <joao.campos@viacometa.com.br>
2026-01-09 11:22:01 -0400

    FEAT: adiciona rota para buscar dados por periodo cnpj

--- src/modules/unimed/unimed.controller.ts
+++ src/modules/unimed/unimed.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { UnimedImportService } from './services/unimed-import.service';
+import { ImportUnimedDto } from './dtos/import-unimed.dto';
 @Controller()
 export class UnimedController {
   constructor(private readonly unimedImportService: UnimedImportService) {}
@@ -7,6 +8,13 @@ export class UnimedController {
   @Get('busca-empresas-unimed')
   async buscaEmpresasUnimed() {
     const result = await this.unimedImportService.buscaEmpresasUnimed();
+    return result;
+  }
+
+  @Get('busca-dados-periodo-cnpj')
+  async buscaDadosCnpj(@Query() params: ImportUnimedDto) {
+    console.log('Parâmetros recebidos:', params);
+    const result = await this.unimedImportService.importarPorCnpj(params);
     return result;
   }
 }
